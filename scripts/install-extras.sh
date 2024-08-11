b2edd706382e44175d46106e7d098ed295d43186
kumarstack55 <kumarstack55@gmail.com>
2024-08-11 20:25:59 +0900

    ansible-playbook を実行するようにした

--- scripts/install-extras.sh
+++ scripts/install-extras.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-sudo apt-get install ansible -y 
+set -x
+
+if [[ ! -f /.install-extras ]]; then
+  sudo apt-get install ansible -y
+
+  (
+    export ANSIBLE_LOCALHOST_WARNING=False
+    export ANSIBLE_INVENTORY_UNPARSED_WARNING=False
+    ansible-playbook --diff /vagrant/ansible/playbook.yml
+  )
+
+  touch /.install-extras
+fi
