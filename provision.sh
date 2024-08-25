a25f7e0a44de44bc7b00cd0e4f07e88d05085731
kumarstack55 <kumarstack55@gmail.com>
2024-08-25 21:38:53 +0900

    exit 1 を加えた

--- provision.sh
+++ provision.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -x
 
-echo "vagrant:vagrant" | sudo chpasswd
+echo "vagrant:vagrant" | sudo chpasswd || exit 1
 
-update-alternatives --set editor /usr/bin/vim.tiny
+update-alternatives --set editor /usr/bin/vim.tiny || exit 1
 
-apt-get update
+apt-get update || exit 1
 
 /vagrant/scripts/install-extras.sh
 /vagrant/scripts/install-gnome-firefox.sh
