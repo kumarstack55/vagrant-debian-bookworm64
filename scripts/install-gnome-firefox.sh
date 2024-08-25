32f71f5f135305e6291ffea18295ece5e9709b36
kumarstack55 <kumarstack55@gmail.com>
2024-08-25 21:54:32 +0900

    ファイルのパスを変数にした

--- scripts/install-gnome-firefox.sh
+++ scripts/install-gnome-firefox.sh
@@ -3,15 +3,17 @@
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 . "${script_dir}/functions.sh"
 
+executed_flag_file_path=/.install-gnome-firefox-executed 
+
 set -x
 
-if [[ ! -f /.install-gnome-firefox-executed ]]; then
+if [[ ! -f "${executed_flag_file_path}" ]]; then
   # OS起動時に GNOME が起動するようにする。
-  # GNOME はコンソールと解像度を設定できるようにする。
+  # GNOME はコンソールと解像度とキーボード・レイアウトを設定できるようにする。
   sudo apt install zstd gnome-console gdm3 -y || die
 
   # Firefox をインストールする。
   sudo apt-get install firefox-esr-l10n-ja -y || die
 
-  sudo touch /.install-gnome-firefox-executed || die
+  sudo touch "${executed_flag_file_path}" || die
 fi
